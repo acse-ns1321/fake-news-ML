02f92f6a56ddab9039a9d7d6f845a038bdf068b6
acse-ns1321 <88569855+acse-ns1321@users.noreply.github.com>
2022-05-08 18:29:37 +0100

    Breast cancer dataset

--- house_price_prediction/challenges.py
+++
@@ -1,7 +0,0 @@
-# Challenges
-# Take reasonable steps to mitigate multicolinearity.
-# Check whether we have met each of the 4 assumptions behind linear regression.
-# If we have not met some of these assumption, is there anything (preferably something simple) that we can do to make sure we do satisfy them? If so, try to implement it.
-# How do you know that you have the correct hyperparemeter k when using k-means? Try using the Elbow method.
-# Can you improve the regression performance by incorporating the clustering results as an additional feature?
-# Can you linearly interpolate across the principal components, reconstructing the corresponding images? What do you notice?
